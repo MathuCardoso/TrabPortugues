9fd207c6629932537eef5963127ea9f6d32aca25
Matheus Cardoso <101215639+MathuCardoso@users.noreply.github.com>
2023-09-04 23:23:50 -0300

    QUAAAAAAAAAAAAAAAAASE

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 var perguntas = document.getElementsByClassName('blocos');
 var reset = document.getElementById('limpar');
 
-reset.onclick = function(){
-    var resposta = document.getElementById('answer');
-    resposta.textContent = '';
-}
-
 for (var i = 0; i < perguntas.length; i++) {
 
-    const respostaContainer = document.getElementById('resposta');
-
     function typeWriter(elemento) {
+
         const textoArray = elemento.innerHTML.split('');
         elemento.innerHTML = '';
         textoArray.forEach((letra, i) => {
-            setTimeout(() => {
-                elemento.innerHTML += letra;
-                respostaContainer.scrollTop = respostaContainer.scrollHeight; // Rola para baixo à medida que o texto é adicionado
-            }, 35 * i);
+          setTimeout(() => elemento.innerHTML += letra, 35 * i);
         });
-    }
+
+      }
+
 
         var resposta = document.getElementById('answer');
        
@@ -51,6 +44,18 @@ for (var i = 0; i < perguntas.length; i++) {
             typeWriter(resposta);
         }else if(pergunta === '8') {
             resposta.textContent = 'A parte mais desafiadora da minha jornada de escrita foi a luta constante para encontrar tempo e recursos para escrever em meio às duras condições da minha vida na favela do Canindé, em São Paulo. Como mãe solteira de três filhos, equilibrar minhas responsabilidades como cuidadora com a escrita era uma tarefa árdua. Encontrar tempo para escrever diariamente era complicado, já que minha prioridade era garantir que meus filhos tivessem o necessário para sobreviver. Além disso, a falta de materiais de escrita, condições de vida precárias na favela, exposição às intempéries e minhas limitações educacionais tornaram a escrita um desafio constante. No entanto, minha determinação em compartilhar as realidades da vida na favela e dar voz aos excluídos me impulsionou a continuar escrevendo. O encontro com o jornalista Audálio Dantas, que me propôs a ideia de publicar meu diário, foi um momento crucial que abriu portas para a divulgação das minhas palavras e experiências. Minha jornada de escrita foi marcada por obstáculos, mas também pela resiliência e pelo desejo de fazer uma diferença na sociedade ao destacar as questões sociais e econômicas que afetavam a população de baixa renda.';
+            typeWriter(resposta);
+        }else if(pergunta === '9') {
+            resposta.textContent = 'Foi uma mistura de emoções intensas. Ver meu livro publicado e reconhecido foi incrível, e o sucesso que ele alcançou foi gratificante. Ele foi traduzido para várias línguas, o que me encheu de orgulho.';
+            typeWriter(resposta);
+        }else if(pergunta === '10') {
+            resposta.textContent = 'O processo de documentar o cotidiano da favela em meu diário foi desafiador, porém essencial para minha missão de dar voz aos marginalizados. Observava atentamente, registrando detalhes das condições de vida, lutas das famílias e interações com moradores. Mantive o hábito de escrever diariamente, muitas vezes à noite, usando papel simples devido à falta de recursos. Minha escrita refletia honestamente as dificuldades e momentos de esperança. Encontrei resistência e apoio na comunidade; alguns viam minha escrita como invasão de privacidade, enquanto outros a encorajavam. O jornalista Audálio Dantas se tornou um importante apoiador. Minha missão era conscientizar o mundo sobre as condições das favelas e dar voz aos marginalizados, acreditando que a escrita poderia promover mudanças sociais. Em resumo, minha jornada de documentação no diário foi desafiadora, mas crucial para chamar a atenção para as questões sociais e econômicas em nossa comunidade, impulsionada por minha determinação e compromisso com a verdade e a justiça.';
+            typeWriter(resposta);
+        }else if(pergunta === '11') {
+            resposta.textContent = 'Escrever na favela era desafiador devido à falta de recursos, como papel e caneta. Mas eu usava o que tinha à mão, muitas vezes escrevendo em pedaços de papel encontrados no lixo, porque acredito que a escrita é uma forma poderosa de expressão.';
+            typeWriter(resposta);
+        }else if(pergunta === '12') {
+            resposta.textContent = 'Escrever na favela era desafiador devido à falta de recursos, como papel e caneta. Mas eu usava o que tinha à mão, muitas vezes escrevendo em pedaços de papel encontrados no lixo, porque acredito que a escrita é uma forma poderosa de expressão.';
             typeWriter(resposta);
         }else {
             resposta.textContent = 'Fala direito filha da puta';
